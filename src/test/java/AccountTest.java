643d2d939c49203847727b627f270f44c5c6adcc
smoskala <sylwestermoskala@gmail.com>
2018-05-15 22:03:36 +0200

    AccountTest

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,19 @@
+
+import com.plgrnds.tests.bank.Account;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+
+/**
+ * Created by sylwe on 15.05.2018.
+ */
+public class AccountTest {
+
+    @Test
+    public void equalsContract() {
+        EqualsVerifier.forClass(Account.class)
+                .usingGetClass()
+                .suppress(Warning.NONFINAL_FIELDS)
+                .verify();
+    }
+}
